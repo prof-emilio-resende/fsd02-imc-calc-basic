27f9f35ca0563e5ace5da5fb6ab99468ca764dbb
Emilio Murta <emilio.murta@msitbrasil.com>
2021-10-14 21:00:00 -0300

    event listeners

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
-function calculateImc() {
-    var weight = parseFloat(document.getElementById("weight").value);
-    var height = parseFloat(document.getElementById("height").value);
+function Person(height, weight) {
+    if (typeof(height) !== "number" || isNaN(height))
+        throw Error("height is not a number");
 
-    var imc = doCalcImc(weight, height);
-    document.getElementById("imc").innerHTML = imc;
+    if (typeof(weight) !== "number" || isNaN(weight))
+        throw Error("height is not a number");
+
+    if (height <= 0 || weight <= 0) 
+        throw Error("height and weight must be bigger than zero");
+
+    this.height = height;
+    this.weight = weight;
 }
 
-function doCalcImc(weight, height) {
-    var imcValue = weight / height ** 2;
-    var imcText = doTranslateImc(imcValue);
-    return imcValue + " " + imcText;
+function Dietician(height, weight) {
+    Person.call(this, height, weight);
+    this.imc = function() {
+        var imcValue = this.weight / this.height ** 2;
+        var imcText = doTranslateImc(imcValue);
+        return imcValue + " " + imcText;
+    }
 }
+
+Dietician.prototype = Object.create(Person.prototype);
+Dietician.prototype.constructor = Dietician;
 
 function doTranslateImc(imc) {
     if (imc < 18.5) return "magreza";
     else if (imc < 24.9) return "normal";
     else if (imc < 30) return "sobrepeso";
     else return "obesidade";
+}
+
+function renderText(text, targetId) {
+    document.getElementById(targetId).innerHTML = text;
+}
+
+function calculateImc() {
+    var weight = parseFloat(document.getElementById("weight").value);
+    var height = parseFloat(document.getElementById("height").value);
+    
+    renderText(new Dietician(height, weight).imc(), "imc");
+}
+
+window.onload = function() {
+    var btn = document.querySelector(".data .form .actions button.primary-action");
+    btn.addEventListener("click", calculateImc);
 }
