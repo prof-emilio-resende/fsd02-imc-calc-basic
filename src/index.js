1f4338ddcb8fd5ac756ed95324f7575f56fb057c
Emilio Murta <emilio.murta@msitbrasil.com>
2021-11-04 22:01:15 -0300

    + webpack

--- src/index.js
+++ src/index.js
@@ -1,31 +1,37 @@
+import "./index.css";
+
 import ImcView from "./views/ImcView.js";
 import ImcController from "./controller/ImcController.js";
 import Person from "./domain/Person.js";
 
-function buildCalculateImc() {
-  var weightElement = document.getElementById("weight");
-  var heightElement = document.getElementById("height");
+(function() {
   var imcView = new ImcView();
-  imcView.onLoad();
   const imcController = new ImcController();
-  const state = imcView.observe({person: new Person(0.1,0.1)});
 
-  return async function (evt) {
-    console.log(evt);
-    var weight = parseFloat(weightElement.value);
-    var height = parseFloat(heightElement.value);
-    var p = new Person(height, weight);
-    var personResult = await imcController.calculate(p);
-    p.imc = personResult.imc;
-    p.imcDescription = personResult.imcDescription;
-    state.person = p;
+  function buildCalculateImc() {
+    var weightElement = document.getElementById("weight");
+    var heightElement = document.getElementById("height");
+    
+    const state = imcView.observe({person: new Person(0.1,0.1)});
+  
+    return async function (evt) {
+      console.log(evt);
+      var weight = parseFloat(weightElement.value);
+      var height = parseFloat(heightElement.value);
+      var p = new Person(height, weight);
+      var personResult = await imcController.calculate(p);
+      p.imc = personResult.imc;
+      p.imcDescription = personResult.imcDescription;
+      state.person = p;
+    };
+  }
+  
+  window.onload = function () {
+    console.log("loading...");
+    var btn = document.querySelector(
+      ".data .form .actions button.primary-action"
+    );
+    btn.addEventListener("click", buildCalculateImc());
+    imcView.onLoad();
   };
-}
-
-window.onload = function () {
-  console.log("loading...");
-  var btn = document.querySelector(
-    ".data .form .actions button.primary-action"
-  );
-  btn.addEventListener("click", buildCalculateImc());
-};
+})();
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require("path")
-const plugins = []
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+const plugins = [new MiniCssExtractPlugin()]
 
 const config = {
   entry: "./src/index.js",
@@ -8,13 +11,39 @@ const config = {
     path: path.resolve(__dirname, "dist")
   },
   module: {
-    rules: []
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
+    ]
   }
 }
 
 module.exports = (env, argv) => {
   if (argv.mode !== "production") {
     config.devtool = "source-map"
+    plugins.push(
+      new HtmlWebpackPlugin({
+        hash: true,
+        minify: false,
+        filename: "index.html",
+        template: path.resolve(__dirname) + "/main.html",
+      })
+    )
+  } else {
+    plugins.push(
+      new HtmlWebpackPlugin({
+        hash: true,
+        minify: {
+          html5: true,
+          collapseWhitespace: true,
+          removeComments: true
+        },
+        filename: "index.html",
+        template: path.resolve(__dirname) + "/main.html",
+      })
+    )
   }
 
   config.plugins = plugins;
